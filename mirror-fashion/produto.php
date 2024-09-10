e26ef5e7011e92fba43d55a7c548f9580b77b75b
magra0 <126424146+Gabsilvaoli@users.noreply.github.com>
2024-09-10 11:36:19 -0300

    Add files via upload

---
+++ mirror-fashion/produto.php
@@ -0,0 +1,55 @@
+<!DOCTYPE	html>
+<html>
+			<head>
+					<meta	charset="utf-8">
+					<meta	name="viewport" content="width=device-width">
+					<title>Mirror	Fashion</title>
+					<link	rel="stylesheet" href="css/reset.css">
+					<link	rel="stylesheet" href="css/principal.css">
+					<link	rel="stylesheet" href="css/produto.css">
+					<link	rel="stylesheet" href="css/mobile.css" media="(max-width:	939px)">
+			</head>
+					<body>
+					<?php	
+					$cabecalho_title	=	"Produto	da	Mirror	Fashion";
+					include("cabecalho.php");	?>
+					<div class="container">
+			<div class="produto">
+				<h1>Fuzzy Cardigan</h1>
+				<p>por apenas R$ 129,00</p>
+
+				<form action="checkout.html">
+			    	<fieldset class="cores">
+			        	<legend>Escolha a cor:</legend>
+
+			          	<input type="radio" name="cor" value="verde" id="verde" checked>
+			          	<label for="verde">
+			            	<img src="img/produtos/foto1-verde.png" alt="verde">
+			         	</label>
+			          
+			          	<input type="radio" name="cor" value="rosa" id="rosa">
+			          	<label for="rosa">
+			            	<img src="img/produtos/foto1-rosa.png" alt="rosa">
+			          	</label>
+			          
+			          	<input type="radio" name="cor" value="azul" id="azul">
+			          	<label for="azul">
+			            	<img src="img/produtos/foto1-azul.png" alt="azul">
+			          	</label>
+			      	</fieldset>
+
+			      	<fieldset class="tamanhos">
+						<legend>Escolha o tamanho:</legend>
+
+						<input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho" oninput="output.value = this.value">
+					 	<output name="output" for="tamanho" value="42">42
+				  	</fieldset>
+
+			      	<input type="submit" class="comprar" value="Comprar">
+			  	</form>
+			</div>
+		</div>
+						<?php	include("rodape.php");	?>
+
+					</body>
+			</html>
